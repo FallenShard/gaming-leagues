f451594c82c8dc77126ba1d86c8e8f5217764fd9
StefanStojanovic <stefko.stojanovic@gmail.com>
2014-11-29 19:41:55 +0100

    Add League entity and LeagueMapping

---
+++ GamingLeagues/GamingLeagues/Entities/League.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamingLeagues.Entities
+{
+    class League
+    {
+        // Primary key
+        public virtual int Id { get; protected set; }
+
+        // Attributes
+        public virtual string Name { get; set; }
+        public virtual DateTime StartDate { get; set; }
+        public virtual DateTime EndDate { get; set; }
+        public virtual float Budget { get; set; }
+
+        // Many-to-one relationship
+        public virtual Game Game { get; set; }
+
+        // Many-to-many relationship
+        public virtual IList<Sponsor> Sponsors { get; set; }
+        //public virtual IList<Player> Players { get; set; }
+
+        // One-to-many relationship
+        public virtual IList<Match> Matches { get; set; }
+
+        public League()
+        {
+            Game = null;
+            Sponsors = new List<Sponsor>();
+            //Players = new List<Player>();
+            Matches = new List<Match>();
+        }
+    }
+}
---
+++ GamingLeagues/GamingLeagues/Mappings/LeagueMapping.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FluentNHibernate.Mapping;
+
+using GamingLeagues.Entities;
+
+namespace GamingLeagues.Mappings
+{
+    class LeagueMapping : ClassMap<League>
+    {
+        public LeagueMapping()
+        {
+            // Primary key mapping
+            Id(x => x.Id);
+
+            // Attribute mapping
+            Map(x => x.Name);
+            Map(x => x.StartDate);
+            Map(x => x.EndDate);
+            Map(x => x.Budget);
+
+            // Many-to-one mapping
+            References(x => x.Game);
+
+            // Many-to-many mapping
+            HasManyToMany(x => x.Sponsors)
+                .Cascade.All()
+                .Table("SponsorsLeague");
+
+            // Many-to-many mapping
+            //HasManyToMany(x => x.Players)
+            //    .Cascade.All()
+            //    .Table("CompetesIn");
+
+            // One-to-many
+            HasMany(x => x.Matches).Inverse().Cascade.All();
+        }
+    }
+}
