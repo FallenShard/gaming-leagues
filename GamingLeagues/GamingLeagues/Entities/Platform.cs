d7cf919455dc9fc3c52ff2b9ccdd41acfcb760fb
FallenShard <nemanjabartolovic@yahoo.com>
2014-11-29 16:01:29 +0100

    Add Platform entity and PlatformMapping

---
+++ GamingLeagues/GamingLeagues/Entities/Platform.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamingLeagues.Entities
+{
+    public class Platform
+    {
+        // Primary key
+        //public virtual int Id { get; protected set; }
+
+        public virtual Game VideoGame { get; set; }
+        public virtual string PlatformTitle { get; set; }
+
+        #region NHibernate Composite Key Requirements
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+
+            var otherPlatform = obj as Platform;
+            if (otherPlatform == null) return false;
+
+            if (VideoGame == otherPlatform.VideoGame && PlatformTitle == otherPlatform.PlatformTitle)
+                return true;
+
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return (VideoGame.Title.ToString() + "|" + PlatformTitle).GetHashCode();
+        }
+        #endregion
+
+        public Platform()
+        {
+            VideoGame = null;
+        }
+    }
+}
---
+++ GamingLeagues/GamingLeagues/Mappings/PlatformMapping.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FluentNHibernate.Mapping;
+
+using GamingLeagues.Entities;
+
+namespace GamingLeagues.Mappings
+{
+    public class PlatformMapping : ClassMap<Platform>
+    {
+        public PlatformMapping()
+        {
+            CompositeId().KeyReference(x => x.VideoGame)
+                         .KeyProperty(x => x.PlatformTitle);
+        }
+    }
+}
