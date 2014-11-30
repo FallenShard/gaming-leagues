958ba6e548158ff4c14d5efa49cdd24802df3a8f
StefanStojanovic <stefko.stojanovic@gmail.com>
2014-11-30 01:20:43 +0100

    Add PlaysInLeague entity and PlaysInLeagueMapping

---
+++ GamingLeagues/GamingLeagues/Mappings/PlaysInLeagueMapping.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FluentNHibernate.Mapping;
+
+using GamingLeagues.Entities;
+
+namespace GamingLeagues.Mappings
+{
+    class PlaysInLeagueMapping : ClassMap<PlaysInLeague>
+    {
+        public PlaysInLeagueMapping()
+        {
+            // Key & Many-to-one mapping
+            CompositeId().KeyReference(x => x.Player)
+                         .KeyReference(x => x.League);
+
+            // Attribute mapping
+            Map(x => x.Points);
+        }
+    }
+}
