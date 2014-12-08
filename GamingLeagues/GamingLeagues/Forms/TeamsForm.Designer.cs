8f8fbdee0c3cfcfafb615f472fc962ce9580ad80
FallenShard <nemanjabartolovic@yahoo.com>
2014-12-08 22:34:30 +0100

    Add Teams Forms

--- GamingLeagues/GamingLeagues/Forms/TeamsForm.Designer.cs
+++ GamingLeagues/GamingLeagues/Forms/TeamsForm.Designer.cs
@@ -28,11 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnDetails = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lvTeams = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(747, 485);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 11;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnDetails
+            // 
+            this.btnDetails.Location = new System.Drawing.Point(747, 101);
+            this.btnDetails.Name = "btnDetails";
+            this.btnDetails.Size = new System.Drawing.Size(75, 23);
+            this.btnDetails.TabIndex = 10;
+            this.btnDetails.Text = "Details";
+            this.btnDetails.UseVisualStyleBackColor = true;
+            this.btnDetails.Click += new System.EventHandler(this.btnDetails_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(747, 72);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(747, 43);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 8;
+            this.btnEdit.Text = "Edit Team";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(747, 14);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Add Team";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lvTeams
+            // 
+            this.lvTeams.FullRowSelect = true;
+            this.lvTeams.GridLines = true;
+            this.lvTeams.Location = new System.Drawing.Point(12, 12);
+            this.lvTeams.Name = "lvTeams";
+            this.lvTeams.Size = new System.Drawing.Size(729, 495);
+            this.lvTeams.TabIndex = 6;
+            this.lvTeams.UseCompatibleStateImageBehavior = false;
+            this.lvTeams.View = System.Windows.Forms.View.Details;
+            // 
+            // TeamsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "TeamsForm";
+            this.ClientSize = new System.Drawing.Size(832, 520);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDetails);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lvTeams);
+            this.Name = "TeamsForm";
+            this.Text = "Teams";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TeamsForm_FormClosing);
+            this.Load += new System.EventHandler(this.TeamsForm_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnDetails;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListView lvTeams;
     }
 }
