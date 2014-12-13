a361d0b4f6347c3a635c5d98fa502050021ff604
StefanStojanovic <stefko.stojanovic@gmail.com>
2014-12-13 15:42:29 +0100

    Add Match Forms

---
+++ GamingLeagues/GamingLeagues/Forms/Matches/MatchesAddForm.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace GamingLeagues.Forms.Matches
+{
+    partial class MatchesAddForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbHomePlayer = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbHomeScore = new System.Windows.Forms.TextBox();
+            this.tbAwayScore = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbAwayPlayer = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtpMatchDate = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Home Player";
+            // 
+            // cmbHomePlayer
+            // 
+            this.cmbHomePlayer.FormattingEnabled = true;
+            this.cmbHomePlayer.Location = new System.Drawing.Point(9, 65);
+            this.cmbHomePlayer.Name = "cmbHomePlayer";
+            this.cmbHomePlayer.Size = new System.Drawing.Size(121, 21);
+            this.cmbHomePlayer.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(133, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Home Score";
+            // 
+            // tbHomeScore
+            // 
+            this.tbHomeScore.Location = new System.Drawing.Point(136, 66);
+            this.tbHomeScore.Name = "tbHomeScore";
+            this.tbHomeScore.Size = new System.Drawing.Size(100, 20);
+            this.tbHomeScore.TabIndex = 3;
+            // 
+            // tbAwayScore
+            // 
+            this.tbAwayScore.Location = new System.Drawing.Point(136, 107);
+            this.tbAwayScore.Name = "tbAwayScore";
+            this.tbAwayScore.Size = new System.Drawing.Size(100, 20);
+            this.tbAwayScore.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(133, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Away Score";
+            // 
+            // cmbAwayPlayer
+            // 
+            this.cmbAwayPlayer.FormattingEnabled = true;
+            this.cmbAwayPlayer.Location = new System.Drawing.Point(9, 106);
+            this.cmbAwayPlayer.Name = "cmbAwayPlayer";
+            this.cmbAwayPlayer.Size = new System.Drawing.Size(121, 21);
+            this.cmbAwayPlayer.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Away Player";
+            // 
+            // dtpMatchDate
+            // 
+            this.dtpMatchDate.Location = new System.Drawing.Point(8, 25);
+            this.dtpMatchDate.Name = "dtpMatchDate";
+            this.dtpMatchDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpMatchDate.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Match Date";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnCancel.Location = new System.Drawing.Point(150, 133);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(122, 50);
+            this.btnCancel.TabIndex = 17;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOk.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnOk.Location = new System.Drawing.Point(8, 133);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(122, 50);
+            this.btnOk.TabIndex = 16;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // MatchesAddForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 191);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dtpMatchDate);
+            this.Controls.Add(this.tbAwayScore);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbAwayPlayer);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbHomeScore);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbHomePlayer);
+            this.Controls.Add(this.label1);
+            this.Name = "MatchesAddForm";
+            this.Text = "MatchesAddForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbHomePlayer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbHomeScore;
+        private System.Windows.Forms.TextBox tbAwayScore;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbAwayPlayer;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dtpMatchDate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
---
+++ GamingLeagues/GamingLeagues/Forms/Matches/MatchesAddForm.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using NHibernate;
+using GamingLeagues.Entities;
+
+namespace GamingLeagues.Forms.Matches
+{
+    public partial class MatchesAddForm : Form
+    {
+        //private ISession m_session;
+
+        private League m_league;
+
+        public MatchesAddForm(/*ISession session, */League league)
+        {
+            InitializeComponent();
+
+            //m_session = session;
+            m_league = league;
+
+            //GetPlayers();
+
+            cmbHomePlayer.Items.Clear();
+            cmbHomePlayer.DataSource = m_league.Players;
+            cmbHomePlayer.DisplayMember = "NameNickLast";
+
+            cmbAwayPlayer.Items.Clear();
+            cmbAwayPlayer.DataSource = m_league.Players;
+            cmbAwayPlayer.DisplayMember = "NameNickLast";
+        }
+
+        /*private void GetPlayers()
+        {
+            IList<Player> players = m_session.CreateQuery("FROM Player").List<Player>();
+            for (int i = 0; i < players.Count; i++)
+                foreach (League league in players[i].Leagues)
+                    if (league.Id == m_league.Id)
+                        m_players.Add(players[i]);
+        }*/
+
+        private void SetAttributes(Match match)
+        {
+            match.DatePlayed = dtpMatchDate.Value;
+            match.HomeScore = int.Parse(tbHomeScore.Text);
+            match.AwayScore = int.Parse(tbAwayScore.Text);
+
+            match.Players.Clear();
+            match.Players.Add(cmbHomePlayer.SelectedItem as Player);
+            match.Players.Add(cmbAwayPlayer.SelectedItem as Player);
+
+            match.League = m_league;
+        }
+
+        private bool ValidateInput()
+        {
+            IList<string> errorMessages = new List<string>();
+
+            DateTime pickedDate = dtpMatchDate.Value;
+            if (DateTime.Now < pickedDate)
+                errorMessages.Add("Match cannot be played in the future.");
+
+            int temp;
+
+            try
+            {
+                temp = int.Parse(tbHomeScore.Text);
+                if (temp < 0)
+                    errorMessages.Add("Home score has to be a positive number");
+            }
+            catch (Exception)
+            {
+                errorMessages.Add("Career earnings has to be a number");
+            }
+
+            try
+            {
+                temp = int.Parse(tbAwayScore.Text);
+                if (temp < 0)
+                    errorMessages.Add("Away score has to be a positive number");
+            }
+            catch (Exception)
+            {
+                errorMessages.Add("Career earnings has to be a number");
+            }
+
+            if (errorMessages.Count == 0)
+                return true;
+            else
+            {
+                string message = "The following errors have been found: " + Environment.NewLine + Environment.NewLine;
+                foreach (string error in errorMessages)
+                    message += "  -  " + error + Environment.NewLine;
+
+                MessageBox.Show(message, Text);
+                return false;
+            }
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            if (ValidateInput())
+            {
+                Match match = new Match();
+                SetAttributes(match);
+
+                ISession session = DataAccessLayer.DataAccessLayer.GetSession();
+
+                try
+                {
+                    session.SaveOrUpdate(match);
+                    session.Flush();
+
+                    this.DialogResult = DialogResult.OK;
+                }
+                catch (Exception saveExc)
+                {
+                    MessageBox.Show("Failed to save player in current session:\n" + saveExc.Message);
+                    this.DialogResult = DialogResult.Cancel;
+                }
+                finally
+                {
+                    session.Close();
+                    Close();
+                }
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to close this window?",
+                Text, MessageBoxButtons.OKCancel) == DialogResult.OK)
+                Close();
+        }
+    }
+}
